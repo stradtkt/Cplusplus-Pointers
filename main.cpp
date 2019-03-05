f9e766c084cd159a79590596f670b1e762a1148c
Kevin <stradtkt22@gmail.com>
2019-03-04 19:04:28 -0500

    Added some pointer code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+void manip(double value) {
+    value = 10.0;
+    cout << "Value of int in manipulate: " << value << endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int nValue = 8;
+    int *pnValue = &nValue;
+
+
+    cout << "nValue: " << nValue << endl;
+    cout << "Pointer nValue: " << pnValue << endl;
+    cout << "De-referenced pointer value: " << *pnValue << endl;
+
+    double dValue = 123.4;
+    cout << "1. dValue: " << dValue << endl;
+    manip(dValue);
+    cout << "4. dValue: " << dValue << endl;
+
+
     return 0;
 }
